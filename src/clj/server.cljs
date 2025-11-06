4192b30d0e73d3f1ae4f628efe9e8b744742be8f
Oliver Marshall <olivershawmarshall@gmail.com>
2025-11-06 18:57:42 +0000

    feat: add basic sse

--- src/clj/server.cljs
+++ src/clj/server.cljs
@@ -3,6 +3,32 @@
 (defn file [path content-type]
   (js/Response. (.file js/Bun path) {:headers {"Content-Type" content-type}}))
 
+(defn sse [req async-gen]
+  (js/Response.
+    (^{:gen true :async true} fn [] (js-yield* (async-gen req)))
+    {:headers {"Content-Type" "text/event-stream"
+               "Cache-Control" "no-cache"
+               "Connection" "keep-alive"}}))
+
+(defn event [msg]
+  (if (string? msg)
+    (str "data: " msg "\n\n")
+    (let [{:keys [event data id retry]} msg]
+      (str (when event (str "event: " event "\n"))
+           (when data (str "data: " data "\n"))
+           (when id (str "id: " id "\n"))
+           (when retry (str "retry: " retry "\n"))
+           "\n\n"))))
+
+(defn ^{:gen true :async true} events [req]
+  (let [last-id (.. req -headers (get "last-event-id"))
+        start (or (some-> last-id js/parseInt inc) 1)]
+    (js-yield (event "Hello World!"))
+    (loop [i start]
+      (js/await (Bun.sleep 1000))
+      (js-yield (event {:data (str i) :id (str i)}))
+      (recur (inc i)))))
+
 (defn handler [req]
   (let [url (js/URL. (.-url req))
         pathname (.-pathname url)]
@@ -10,6 +36,7 @@
       #"/" (file "public/index.html" "text/html")
       #"/app\.js" (file "app.js" "application/javascript")
       #"/node_modules/.*" (file (str ".." pathname) "application/javascript")
+      #"/api/sse" (sse req events)
       (js/Response. "Not Found" #js {:status 404}))))
 
 (def server (.serve js/Bun {:port 3000 :fetch handler}))
--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -1,11 +1,36 @@
 (ns app
   (:require ["https://esm.sh/reagami" :as reagami]))
 
+
+;; >> App
+
 (def state (atom {:counter 0}))
 
 (defn app []
   (let [{:keys [counter]} @state]
     [:h1 "Counter: " counter]))
+
+
+
+;; >> SSE Client
+
+(defn listen
+  ([event-source f]
+   (listen event-source "message" f))
+  ([event-source event f]
+   (.addEventListener event-source event f)))
+
+(let [event-source (js/EventSource. "/api/sse")]
+  (listen event-source "message"
+          (fn [{:keys [data]}]
+            (js/console.log "Received event via listen:" data)
+            (let [counter (js/parseInt data)]
+              (when (not (js/isNaN counter))
+                (swap! state assoc :counter counter))))))
+
+
+
+;; >> Render Loop
 
 (defn render []
   (reagami/render (js/document.getElementById "app") [app]))
